e25efe8a974892a70ef73828368ee06fd5a4d0bd
Pablo <111327616+paul086@users.noreply.github.com>
2023-03-29 22:14:01 -0300

    Create Cancion.java

---
+++ EjExtras1POO/ejextras1poo.Canciones/Cancion.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejextras1poo.Canciones;
+
+/**
+ *
+ * @author pablcastro
+ */
+public class Cancion {
+
+    public String titulo;
+    public String autor;
+
+    public Cancion() {
+        this.titulo = "";
+        this.autor = "";
+    }
+
+    public Cancion(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" + "titulo=" + titulo + ", autor=" + autor + '}';
+    }
+}
