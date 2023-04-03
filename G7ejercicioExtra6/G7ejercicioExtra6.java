bea442ae8439196cf1e16cda7678e860d788f3f1
Pablo <111327616+paul086@users.noreply.github.com>
2023-04-03 20:18:01 -0300

    Create G7ejercicioExtra6.java

---
+++ G7ejercicioExtra6/G7ejercicioExtra6.java
@@ -0,0 +1,13 @@
+package ejextras6poo;
+
+
+
+public class G7ejercicioExtra6 {
+
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        empleado1.crearEmpleado();
+        empleado1.calcular_aumento();
+    }
+    
+}
