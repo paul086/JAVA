7e511a9a21250cdd7ef2af9c5d0ac0a0c1c59e8b
Pablo <111327616+paul086@users.noreply.github.com>
2023-04-11 08:54:25 -0300

    Create CuentaBancaria.java

---
+++ Guia1Servicios/Guia8Ej1/Entidades/CuentaBancaria.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author pablo
+ */
+public class CuentaBancaria {
+
+
+    private int numeroCuenta;
+    private long dniCliente;
+    private double saldoActual = 1000000;
+
+    public CuentaBancaria() {
+    }
+
+    public CuentaBancaria(int numeroCuenta, long dniCliente, double saldoActual) {
+        this.numeroCuenta = numeroCuenta;
+        this.dniCliente = dniCliente;
+        this.saldoActual = saldoActual;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public long getDniCliente() {
+        return dniCliente;
+    }
+
+    public void setDniCliente(long dniCliente) {
+        this.dniCliente = dniCliente;
+    }
+
+    public double getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria{" + "numeroCuenta=" + numeroCuenta + ", dniCliente=" + dniCliente + ", saldoActual=" + saldoActual + '}';
+    }
+    
+    }
+
+
+
